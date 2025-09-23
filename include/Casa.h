b0516c21bcd6e67092a2044ecf925b7501029560
Riquelmy Debona <Riquelmydebona@hotmail.com>
2025-09-23 20:59:55 -0300

    Chaminé e fumaça (agora sim)

--- include/Casa.h
+++ include/Casa.h
@@ -1,16 +1,14 @@
 #ifndef CASA_H
 #define CASA_H
 
+#include "HtmlColor.h"
 #include <SDL2/SDL.h>
 #include "Shape.h"
 #include "Primitives.h"
-#include "Point.h"
-#include "HTMLColor.h"
 #include <string>
-#include <vector>
 
 class Casa : public Shape {
-private:
+protected:
     int altura, largura;
     double inclinacao;
     Point posic;
@@ -19,84 +17,68 @@ private:
 
 public:
     Casa() {
-        pontosParede = {
-            {0, 0}, {0, 0.5}, {1, 0.5}, {1, 0}
-        };
-
-        pontosTeto = {
-            {0, 0.5}, {0.5, 1}, {1, 0.5}
-        };
-
-        pontosPorta = {
-            {0.35, 0}, {0.35, 0.3}, {0.65, 0.3}, {0.65, 0}
-        };
-
-        // Chaminé no lado esquerdo do telhado
-        pontosChamine = {
-            {0.15, 0.5}, {0.25, 0.5}, {0.25, 0.8}, {0.15, 0.8}
-        };
-
         altura = 1;
         largura = 1;
         inclinacao = 0;
     }
 
-std::vector<Point> fumacaRelativa = {
-    {0.2, 1.0},
-    {0.15, 1.3},
-    {0.1, 1.5}
-};
-
+    // fumaça agora sobe para cima (y positivo)
+    std::vector<Point> fumacaRelativa = {
+        {0.2, 0.8},
+        {0.15, 0.9},
+        {0.1, 1.0}
+    };
 
     void draw(SDL_Surface* surface, World mundo) override {
         if (!surface) return;
+
         HtmlColor hc;
 
-        // --- Parede ---
-        std::vector<Point> paredeTela = pontosParede;
-        Primitives::scalePolygon(paredeTela, largura, altura, 0, 0);
-        Primitives::translatePolygon(paredeTela, posic.getX(), posic.getY());
-        Primitives::rotatePolygon(paredeTela, inclinacao, posic.getX(), posic.getY());
-        for (auto& p : paredeTela) p = mundo.worldToScreen(p);
+        // --- PAREDE ---
+        Uint32 corParedeTela = hc.getColorUint32(corParede, surface);
+        std::vector<Point> parede = {{0.1,0}, {0.9,0}, {0.9,0.5}, {0.1,0.5}};
+        Primitives::scalePolygon(parede, largura, altura, 0, 0);
+        Primitives::translatePolygon(parede, posic.getX(), posic.getY());
+        Primitives::rotatePolygon(parede, inclinacao, posic.getX(), posic.getY());
+        for (auto &p : parede) p = mundo.worldToScreen(p);
+        Primitives::drawPolygon(surface, parede, corParedeTela);
+        Primitives::scanFill(surface, parede, corParedeTela);
 
-        Uint32 corParedeI = hc.getColorUint32(corParede, surface);
-        Primitives::drawPolygon(surface, paredeTela, corParedeI);
-        Primitives::scanFill(surface, paredeTela, corParedeI);
+        // --- TELHADO ---
+        Uint32 corTelhadoTela = hc.getColorUint32(corTelhado, surface);
+        std::vector<Point> telhado = {{0,0.5}, {1,0.5}, {0.5,0.8}};
+        Primitives::scalePolygon(telhado, largura, altura, 0, 0);
+        Primitives::translatePolygon(telhado, posic.getX(), posic.getY());
+        Primitives::rotatePolygon(telhado, inclinacao, posic.getX(), posic.getY());
+        for (auto &p : telhado) p = mundo.worldToScreen(p);
+        Primitives::drawPolygon(surface, telhado, corTelhadoTela);
+        Primitives::scanFill(surface, telhado, corTelhadoTela);
 
-        // --- Telhado ---
-        std::vector<Point> tetoTela = pontosTeto;
-        Primitives::scalePolygon(tetoTela, largura, altura, 0, 0);
-        Primitives::translatePolygon(tetoTela, posic.getX(), posic.getY());
-        Primitives::rotatePolygon(tetoTela, inclinacao, posic.getX(), posic.getY());
-        for (auto& p : tetoTela) p = mundo.worldToScreen(p);
+        // --- PORTA ---
+        Uint32 corPortaTela = hc.getColorUint32(corPorta, surface);
+        std::vector<Point> porta = {{0.4,0}, {0.6,0}, {0.6,0.25}, {0.4,0.25}};
+        Primitives::scalePolygon(porta, largura, altura, 0, 0);
+        Primitives::translatePolygon(porta, posic.getX(), posic.getY());
+        Primitives::rotatePolygon(porta, inclinacao, posic.getX(), posic.getY());
+        for (auto &p : porta) p = mundo.worldToScreen(p);
+        Primitives::drawPolygon(surface, porta, corPortaTela);
+        Primitives::scanFill(surface, porta, corPortaTela);
 
-        Uint32 corTelhadoI = hc.getColorUint32(corTelhado, surface);
-        Primitives::drawPolygon(surface, tetoTela, corTelhadoI);
-        Primitives::scanFill(surface, tetoTela, corTelhadoI);
+        // --- CHAMINÉ ---
+        Uint32 corChamineTela = hc.getColorUint32(corChamine, surface);
+        std::vector<Point> chamine = {
+            {0.15,0.5}, {0.28,0.5}, {0.28,0.7}, {0.15,0.7}
+        };
+        Primitives::scalePolygon(chamine, largura, altura, 0, 0);
+        Primitives::translatePolygon(chamine, posic.getX(), posic.getY());
+        Primitives::rotatePolygon(chamine, inclinacao, posic.getX(), posic.getY());
+        for (auto &p : chamine) p = mundo.worldToScreen(p);
+        Primitives::drawPolygon(surface, chamine, corChamineTela);
+        Primitives::scanFill(surface, chamine, corChamineTela);
 
-        // --- Porta ---
-        std::vector<Point> portaTela = pontosPorta;
-        Primitives::scalePolygon(portaTela, largura, altura, 0, 0);
-        Primitives::translatePolygon(portaTela, posic.getX(), posic.getY());
-        Primitives::rotatePolygon(portaTela, inclinacao, posic.getX(), posic.getY());
-        for (auto& p : portaTela) p = mundo.worldToScreen(p);
-
-        Uint32 corPortaI = hc.getColorUint32(corPorta, surface);
-        Primitives::drawPolygon(surface, portaTela, corPortaI);
-        Primitives::scanFill(surface, portaTela, corPortaI);
-
-        // --- Chaminé ---
-        std::vector<Point> chamineTela = pontosChamine;
-        Primitives::scalePolygon(chamineTela, largura, altura, 0, 0);
-        Primitives::translatePolygon(chamineTela, posic.getX(), posic.getY());
-        Primitives::rotatePolygon(chamineTela, inclinacao, posic.getX(), posic.getY());
-        for (auto& p : chamineTela) p = mundo.worldToScreen(p);
-
-        Uint32 corChamineI = hc.getColorUint32(corChamine, surface);
-        Primitives::drawPolygon(surface, chamineTela, corChamineI);
-        Primitives::scanFill(surface, chamineTela, corChamineI);
-
-        double raioBaseSRU = 0.07;
+        // --- FUMAÇA ---
+        Uint32 corFumacaTela = hc.getColorUint32(corFumaca, surface);
+        double raioBaseSRU = 0.07; // fumaça menor e mais realista
 
         for (int i = 0; i < (int)fumacaRelativa.size(); i++) {
             Point centro = Primitives::scalePoint(fumacaRelativa[i], largura, altura, 0, 0);
@@ -104,28 +86,27 @@ std::vector<Point> fumacaRelativa = {
             centro = Primitives::rotatePoint(centro, inclinacao, posic.getX(), posic.getY());
             centro = mundo.worldToScreen(centro);
 
-            // raio menor a cada círculo
+            // raio decresce a cada círculo
             double raioSRU = raioBaseSRU - i * 0.02;
             int raioTela = mundo.worldToScreen(Primitives::scaleLength(raioSRU, largura, altura));
 
-            Uint32 corFumacaI = hc.getColorUint32(corFumaca, surface);
-            Primitives::drawCircle(surface, centro.getX(), centro.getY(), raioTela, corFumacaI);
-            Primitives::floodFill(surface, centro.getX(), centro.getY(), corFumacaI, corFumacaI);
+            Primitives::drawCircle(surface, centro.getX(), centro.getY(), raioTela, corFumacaTela);
+            Primitives::floodFill(surface, centro.getX(), centro.getY(), corFumacaTela, corFumacaTela);
+
         }
     }
 
-    // --- Getters ---
+    // --- Getters/Setters ---
     Point getPosicao() const { return posic; }
     int getAltura() const { return altura; }
     int getLargura() const { return largura; }
+    double getInclinacao() const { return inclinacao; }
     std::string getCorParede() const { return corParede; }
     std::string getCorTelhado() const { return corTelhado; }
     std::string getCorPorta() const { return corPorta; }
     std::string getCorChamine() const { return corChamine; }
     std::string getCorFumaca() const { return corFumaca; }
-    double getInclinacao() const { return inclinacao; }
 
-    // --- Setters ---
     void setPosic(Point ponto) { posic = ponto; }
     void setAltura(int valor) { altura = valor; }
     void setLargura(int valor) { largura = valor; }
